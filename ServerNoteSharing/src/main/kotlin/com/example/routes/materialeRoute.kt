2798237c692cc6dfc1625efd6b79983c390f112b
Ylli <ybraci@studenti.uninsubria.it>
2024-05-10 17:41:12 +0200

    Sistemazione metodo POST

--- ServerNoteSharing/src/main/kotlin/com/example/routes/materialeRoute.kt
+++ ServerNoteSharing/src/main/kotlin/com/example/routes/materialeRoute.kt
@@ -2,9 +2,12 @@ package com.example.routes
 
 import com.example.data.MaterialeFisico
 import io.ktor.http.*
+import io.ktor.http.content.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.io.File
 
 fun Route.materialeRoute() {
     get("/materiale") {
@@ -13,5 +16,32 @@ fun Route.materialeRoute() {
             MaterialeFisico(20, 1999, "Analisi matematica 1", "Appunti primo anno analisi matematica", "Varese", "Casorate Sempione", "Via Trieste 76", 21011
                 )
         )
+    }
+
+    post("/upload") {
+        val multipart = call.receiveMultipart()
+        var fileBytes: ByteArray? = null
+        var fileName: String? = null
+
+        multipart.forEachPart { part ->
+            when (part) {
+                is PartData.FileItem -> {
+                    fileBytes = part.streamProvider().readBytes()
+                    fileName = part.originalFileName
+                }
+                // Handle other form fields if needed
+                else -> {
+                    // Do nothing or handle other fields
+                }
+            }
+        }
+
+        // Save the file if needed
+        fileBytes?.let {
+            val file = File("C:/Users/ilibr/Downloads/$fileName.pdf")
+            file.writeBytes(it)
+        }
+
+        call.respond(HttpStatusCode.OK, "File uploaded successfully")
     }
 }
