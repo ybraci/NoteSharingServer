d5682d13c020453b2d4bad55987e5f9734da1da6
Ylli <ybraci@studenti.uninsubria.it>
2024-05-13 11:01:31 +0200

    Sistemazione creazione istanza db

--- ServerNoteSharing/src/main/kotlin/com/example/Application.kt
+++ ServerNoteSharing/src/main/kotlin/com/example/Application.kt
@@ -6,7 +6,9 @@ import com.example.plugins.configureRouting
 import com.example.plugins.configureSerialization
 import io.ktor.server.application.*
 
+
 fun main(args: Array<String>) {
+    /*
     print("Host db?: ")
     val host: String? = readLine()
     print("Nome db?: ")
@@ -16,14 +18,16 @@ fun main(args: Array<String>) {
     print("Password db?: ")
     val password: String? = readLine()
     val database: Database = Database
-
     Database.getInstance(host!!, nomedb!!, user!!, password!!)
+     */
 
     io.ktor.server.netty.EngineMain.main(args)
     //embeddedServer(Netty, port = 8080) {module(database)}.start(wait = true)}
 }
 
-fun Application.module(database: Database) {
+fun Application.module() {
+    val database: Database = Database
+    Database.getInstance("localhost", "db_noteSharing", "postgres", "postgres")
     configureSerialization()
     configureMonitoring()
     configureRouting(database)
